62c2b0d256f3a66c6ed898bf5b0bc897f42afab3
gastoire <buzzcutseason73@gmail.com>
2022-05-29 03:03:50 -0300

    Removido React para iniciar version con VUE

--- frontend/src/app/axiosClient.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const axiosClient = axios.create({
-  baseURL: "http://127.0.0.1:4000/api",
-});
-
-export default axiosClient;
--- frontend/src/app/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import projectsReducer from "./reducers/projectsReducer";
-import usersReducer from "./reducers/usersReducer";
-
-export default combineReducers({
-  projects: projectsReducer,
-  users: usersReducer,
-});
